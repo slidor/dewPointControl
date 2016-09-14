a5ae3817908cc382926c1ba1a7b11c815277a90e
Sebastian Kern <sebastian.kern@methodpark.de>
2016-09-14 11:30:49 +0200

    add comments and print statements

--- dewPointControl.py
+++ dewPointControl.py
@@ -8,18 +8,24 @@ import Adafruit_DHT
 import dweepy
 import RPi.GPIO as GPIO
 
+# setup sensor type
 outdoorSensor = Adafruit_DHT.DHT22
+indoorSensor = Adafruit_DHT.DHT22
+
+#define outdoor sensor GPIO pin
 outdoorSensorPin = 3
 
-indoorSensor = Adafruit_DHT.DHT22
+#define indoor sensor GPIO pin
 indoorSensorPin = 5
 
+#define relais GPIO pin
 relaisPin = 7
 
-GPIO.setmode(GPIO.BOARD)
 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(relaisPin, GPIO.OUT)
 
+# define global variables
 dewPointLowerBorder = 2
 dewPointUpperBorder = 9
 minIndoorTemp = 10
@@ -38,19 +44,25 @@ def getserial():
   return cpuserial
 
 def startVentilation():
+    print("startVentilation")
     GPIO.output(relaisPin, GPIO.HIGH)
 
 def stopVentilation():
+    print("stopVentilation")
     GPIO.output(relaisPin, GPIO.LOW)
 
 def readSensorData():
     indoorHumidity, indoorTemperature = Adafruit_DHT.read.retry(indoorSensor, indoorSensorPin)
+    print("indoorHumidity: " + indoorHumidity + "%")
+    print("indoorTemperature: " + indoorTemperature + "°C")
     oudoorHumidity, oudoorTemperature = Adafruit_DHT.read.retry(outdoorSensor, outdoorSensorPin)
+    print("oudoorHumidity: " + outdoorHumidity + "%")
+    print("outdoorTemperature: " + outdoorTemperature + "°C")
     return(indoorHumidity, indoorTemperature, oudoorHumidity, oudoorTemperature)
 
 def calculateDewPointTemperature(relHumidity, temperature):
 	#relHumidity in percent
-	return ((relHumidity / 100) ** (1 / 8.02)) * (109.8 + temperature) - 109.8 
+	return ((relHumidity / 100) ** (1 / 8.02)) * (109.8 + temperature) - 109.8
 
 def ventilationMakesSense():
     result = false
@@ -58,19 +70,23 @@ def ventilationMakesSense():
 
     indoorHumidity, indoorTemperature, oudoorHumidity, oudoorTemperature = readSensorData()
     indoorDewPoint = calculateDewPointTemperature(indoorHumidity, indoorTemperature)
+    print("indoorDewPoint: " + indoorDewPoint + "°C")
     outdoorDewPoint = calculateDewPointTemperature(oudoorHumidity, oudoorTemperature)
-	
+    print("outdoorDewPoint: " + outdoorDewPoint + "°C")
+
 	dewPointDiff = indoorDewPoint - outdoorDewPoint
-	
+
     if (indoorTemperature > minIndoorTemp && (dewPointDiff >= dewPointLowerBorder || dewPointDiff <= dewPointUpperBorder):
-    	return true
+    	result = true
+
+    print("ventilationMakesSense: " + result)
 
     # Send sensor deta and results to dweet.io
     dweepy.dweet_for(thingname, {'indoorHumidity': indoorHumidity, 'indoorTemperature': indoorTemperature, 'indoorDewPoint': indoorDewPoint, 'oudoorHumidity': oudoorHumidity, 'oudoorTemperature': oudoorTemperature, 'outdoorDewPoint': outdoorDewPoint, 'ventilation': result})
 
     return result
 
-if (ventilationMakesSense):
+if (ventilationMakesSense()):
     startVentilation()
 else:
     stopVentilation()
